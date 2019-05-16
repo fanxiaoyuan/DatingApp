b681d1a3308cc779be80e6ac514a12eab146d54b
Xiaoyuan Fan <xiaoyuanfxy@gmail.com>
2019-05-16 22:30:29 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,7 +19,8 @@ namespace DatingApp.API.Controllers
         {
             _context = Context;
         }
-        // GET api/values
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
